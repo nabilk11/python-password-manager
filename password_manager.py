2c9576e8fd446080da87f720eb3a480a7ce09855
nabilk11 <khan.nabiln11@gmail.com>
2022-03-14 23:15:14 -0400

    add and view functions, can now add or read password.txt file

---
+++ password_manager.py
@@ -0,0 +1,31 @@
+# Password Manager
+
+master_pwd = input("What is the Master Password? ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
+
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+# 'a' is the mode 'w' - overwrite the file with new data 'a' - add to the end of file, and creates one if its not there 'r' - read only
+    with open('passwords.txt', 'a') as f:  # file = open('filename.txt') - opens a file.close('filename.txt') - closes
+        f.write(name + "|" + pwd + "\n")        # using with bc it will close the file for you
+                                        #as f is naming it all f
+
+while True:
+    mode = input("Add a New Password? or View Existing Password? Or Type Q to Quit. (add or view) ").lower()
+    if mode in ["q", "quit"]:
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid Input...")
+        continue
