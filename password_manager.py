033a39c647d878a927bb01dd40a26643d40a7dc8
nabilk11 <khan.nabiln11@gmail.com>
2022-03-15 11:57:33 -0400

    installed cryptography and added base key generation code

--- password_manager.py
+++ password_manager.py
@@ -1,6 +1,14 @@
 # Password Manager
+from cryptography.fernet import Fernet
+
 
 master_pwd = input("What is the Master Password? ")
+
+# Write Enc Key
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
 
 def view():
     with open('passwords.txt', 'r') as f:
