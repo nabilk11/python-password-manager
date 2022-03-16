46aaf8f3b3f5d08456e502fbb6c9281a79ab3689
nabilk11 <khan.nabiln11@gmail.com>
2022-03-15 20:55:20 -0400

    added key generation function, generated key.key file with key

---
+++ keywords.js
@@ -0,0 +1,27 @@
+const locations = [
+    "hasbrouck heights",
+    "wood ridge",
+    "carlstadt",
+    "rutherford",
+    "east rutherford",
+    "moonachie",
+    "hackensack",
+    "south hackensack",
+    "Bogota",
+    "Garfield",
+    "teterboro",
+    "maywood",
+    "wallington",
+    "lodi",
+    "little ferry",
+    "rochelle park",
+    "ridgefield park",
+    "saddle brook",
+    "lyndhurst",
+    "elmwood park",
+    "passaic"
+]
+
+locations.map((location) => {
+    console.log("italian food " + location)
+})
